d24af6fd5f955f155f5c821e89266ae1739b74f7
arnoldkajolli <akajolli@vivere.al>
2024-11-26 16:23:47 +0000

    new

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" a script that adds two arrays  """
+
+
+def add_arrays(arr1, arr2):
+    """ a script that adds two arrays  """
+    if len(arr1) != len(arr2):
+        return None
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
+
+
+
