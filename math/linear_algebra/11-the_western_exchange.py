a5c30e7f74b718adf1767df172f24be33f396203
arnoldkajolli <akajolli@vivere.al>
2024-11-28 22:47:14 +0000

    mew

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Contains a function that transposes a numpy.ndarray"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a numpy.ndarray
+    Args:
+        matrix: numpy.ndarray to transpose
+    Returns:
+        New transposed numpy.ndarray
+    """
+    return matrix.transpose()
