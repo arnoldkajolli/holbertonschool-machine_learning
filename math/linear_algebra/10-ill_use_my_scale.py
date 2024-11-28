3ae94f59d3cfe10a82f74fecaa09e5b8dbbf5caa
arnoldkajolli <akajolli@vivere.al>
2024-11-28 22:46:08 +0000

    ntt

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Contains a function that calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray
+    Args:
+        matrix: numpy.ndarray
+    Returns:
+        Tuple of integers representing the shape of matrix
+    """
+    return matrix.shape
