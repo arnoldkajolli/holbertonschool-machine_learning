a02ba97429e465e0cc694213bd1f71dae8ff21c3
arnoldkajolli <akajolli@vivere.al>
2025-02-20 22:56:30 +0000

    new

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """Module for Normal distribution class"""
 
+import math
+
 
 class Normal:
     """Class that represents a normal distribution"""
 
     def __init__(self, data=None, mean=0., stddev=1.):
         """
-        Initialize Normal distribution
+        Initialize Normal distribution.
         Args:
             data: list of data to estimate distribution
             mean: mean of the distribution
@@ -23,15 +25,13 @@ class Normal:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-
             self.mean = float(sum(data) / len(data))
-            squared_diff_sum = sum((x - self.mean) ** 2 for x in data)
-            # Use the unbiased estimator: divide by (n - 1)
-            self.stddev = float((squared_diff_sum / (len(data) - 1)) ** 0.5)
+            variance = sum((x - self.mean) ** 2 for x in data) / (len(data) - 1)
+            self.stddev = math.sqrt(variance)
 
     def z_score(self, x):
         """
-        Calculates the z-score of a given x-value
+        Calculates the z-score of a given x-value.
         Args:
             x: x-value
         Returns:
@@ -41,55 +41,47 @@ class Normal:
 
     def x_value(self, z):
         """
-        Calculates the x-value of a given z-score
+        Calculates the x-value of a given z-score.
         Args:
             z: z-score
         Returns:
-            x-value of z
+            x-value corresponding to z
         """
         return self.mean + z * self.stddev
 
     def pdf(self, x):
         """
-        Calculates the value of the PDF for a given x-value
+        Calculates the value of the PDF for a given x-value.
         Args:
             x: x-value
         Returns:
             PDF value for x
         """
-        pi = 3.1415926536
-        e = 2.7182818285
+        pi = math.pi
+        e = math.e
         exponent = -0.5 * ((x - self.mean) / self.stddev) ** 2
-        coefficient = 1 / (self.stddev * (2 * pi) ** 0.5)
+        coefficient = 1 / (self.stddev * math.sqrt(2 * pi))
         return coefficient * (e ** exponent)
 
     def cdf(self, x):
         """
-        Calculates the value of the CDF for a given x-value
+        Calculates the value of the CDF for a given x-value.
+        Uses a common polynomial approximation.
         Args:
             x: x-value
         Returns:
             CDF value for x
         """
         z = (x - self.mean) / self.stddev
-        # Constants for the approximation
-        b = [0.31938153, -0.356563782, 1.781477937,
-             -1.821255978, 1.330274429]
-        p = 0.2316419
-
-        # Calculate the approximation based on |z|
-        z_abs = abs(z)
-        t = 1.0 / (1.0 + p * z_abs)
-        sum_term = t * (b[0] +
-                        t * (b[1] +
-                             t * (b[2] +
-                                  t * (b[3] +
-                                       t * b[4]))))
-        # Compute the standard normal PDF value
-        pdf_val = 1.0 / ((2 * 3.1415926536) ** 0.5) * (2.7182818285 ** (-0.5 * z_abs * z_abs))
-        result = 1.0 - pdf_val * sum_term
-
-        if z < 0:
-            result = 1.0 - result
-
-        return result
+        t = 1 / (1 + 0.2316419 * abs(z))
+        poly = (0.31938153 * t -
+                0.356563782 * t ** 2 +
+                1.781477937 * t ** 3 -
+                1.821255978 * t ** 4 +
+                1.330274429 * t ** 5)
+        approx = (1 / math.sqrt(2 * math.pi)) * math.exp(-0.5 * z ** 2) * poly
+        if z >= 0:
+            cdf = 1 - approx
+        else:
+            cdf = approx
+        return cdf
