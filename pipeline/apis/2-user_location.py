6026a02796216f7e124fcb087b1c286f6cd3723a
arnoldkajolli <akajolli@vivere.al>
2025-02-21 11:42:24 +0000

    new

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,32 +1,13 @@
 #!/usr/bin/env python3
-"""
-GitHub API:
-Script that prints the location of a specific user
-"""
-
-
-import requests
+"""Script that displays user location from an API"""
 import sys
-import time
-
-
-DEFAULT_HEADERS = {"Accept": "application/vnd.github.v3+json"}
+import requests
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        exit()
+        sys.exit(1)
+        
     url = sys.argv[1]
-    r = requests.get(url, DEFAULT_HEADERS)
-
-    if r.status_code == 200:
-        print(r.json()["location"])
-
-    elif r.status_code == 404:
-        print("Not found")
-
-    elif r.status_code == 403:
-        rate_limit = int(r.headers["X-Ratelimit-Reset"])
-        now = int(time.time())
-        minutes = int((rate_limit - now) / 60)
-        print("Reset in {} min".format(minutes))
+    response = requests.get(url).json()
+    print(response.get('location', ''))
