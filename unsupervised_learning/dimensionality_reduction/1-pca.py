3c267917bc9f6c14077d8b9236b4c2a0608c8c34
arnoldkajolli <akajolli@vivere.al>
2025-02-21 14:00:59 +0000

    new

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -8,15 +8,6 @@ import numpy as np
 def pca(X, ndim):
     """
     Performs PCA on a dataset to reduce dimensions.
-
-    Args:
-        X: numpy.ndarray of shape (n, d) where:
-            n is the number of data points
-            d is the number of dimensions in each point
-        ndim: new dimensionality of the transformed X
-
-    Returns:
-        T: numpy.ndarray of shape (n, ndim) containing the transformed version of X
     """
     # Center the data by subtracting the mean
     X_mean = X - np.mean(X, axis=0)
